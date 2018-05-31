53979205c4084483c0c5163698e3fc92a10d7ee3
race brocx <race.brocx@nordstrom.com>
2018-05-31 09:24:02 -0700

    init

--- index.js
+++ index.js
@@ -2,23 +2,26 @@
  * A Bot for Slack!
  */
 
-
 /**
  * Define a function for initiating a conversation on installation
  * With custom integrations, we don't have a way to find out who installed us, so we can't message them :(
  */
 
+
+
 function onInstallation(bot, installer) {
-    if (installer) {
-        bot.startPrivateConversation({user: installer}, function (err, convo) {
-            if (err) {
-                console.log(err);
-            } else {
-                convo.say('I am a bot that has just joined your team');
-                convo.say('You must now /invite me to a channel so that I can be of use!');
-            }
-        });
-    }
+  if (installer) {
+    bot.startPrivateConversation({
+      user: installer
+    }, function(err, convo) {
+      if (err) {
+        console.log(err);
+      } else {
+        convo.say('I am a bot that has just joined your team');
+        convo.say('You must now /invite me to a channel so that I can be of use!');
+      }
+    });
+  }
 }
 
 
@@ -28,14 +31,16 @@ function onInstallation(bot, installer) {
 
 var config = {};
 if (process.env.MONGOLAB_URI) {
-    var BotkitStorage = require('botkit-storage-mongo');
-    config = {
-        storage: BotkitStorage({mongoUri: process.env.MONGOLAB_URI}),
-    };
+  var BotkitStorage = require('botkit-storage-mongo');
+  config = {
+    storage: BotkitStorage({
+      mongoUri: process.env.MONGOLAB_URI
+    }),
+  };
 } else {
-    config = {
-        json_file_store: ((process.env.TOKEN)?'./db_slack_bot_ci/':'./db_slack_bot_a/'), //use a different name if an app or CI
-    };
+  config = {
+    json_file_store: ((process.env.TOKEN) ? './db_slack_bot_ci/' : './db_slack_bot_a/'), //use a different name if an app or CI
+  };
 }
 
 /**
@@ -43,17 +48,17 @@ if (process.env.MONGOLAB_URI) {
  */
 
 if (process.env.TOKEN || process.env.SLACK_TOKEN) {
-    //Treat this as a custom integration
-    var customIntegration = require('./lib/custom_integrations');
-    var token = (process.env.TOKEN) ? process.env.TOKEN : process.env.SLACK_TOKEN;
-    var controller = customIntegration.configure(token, config, onInstallation);
+  //Treat this as a custom integration
+  var customIntegration = require('./lib/custom_integrations');
+  var token = (process.env.TOKEN) ? process.env.TOKEN : process.env.SLACK_TOKEN;
+  var controller = customIntegration.configure(token, config, onInstallation);
 } else if (process.env.CLIENT_ID && process.env.CLIENT_SECRET && process.env.PORT) {
-    //Treat this as an app
-    var app = require('./lib/apps');
-    var controller = app.configure(process.env.PORT, process.env.CLIENT_ID, process.env.CLIENT_SECRET, config, onInstallation);
+  //Treat this as an app
+  var app = require('./lib/apps');
+  var controller = app.configure(process.env.PORT, process.env.CLIENT_ID, process.env.CLIENT_SECRET, config, onInstallation);
 } else {
-    console.log('Error: If this is a custom integration, please specify TOKEN in the environment. If this is an app, please specify CLIENTID, CLIENTSECRET, and PORT in the environment');
-    process.exit(1);
+  console.log('Error: If this is a custom integration, please specify TOKEN in the environment. If this is an app, please specify CLIENTID, CLIENTSECRET, and PORT in the environment');
+  process.exit(1);
 }
 
 
@@ -66,43 +71,86 @@ if (process.env.TOKEN || process.env.SLACK_TOKEN) {
  * TODO: fixed b0rked reconnect behavior
  */
 // Handle events related to the websocket connection to Slack
-controller.on('rtm_open', function (bot) {
-    console.log('** The RTM api just connected!');
+controller.on('rtm_open', function(bot) {
+  console.log('** The RTM api just connected!');
 });
 
-controller.on('rtm_close', function (bot) {
-    console.log('** The RTM api just closed');
-    // you may want to attempt to re-open
+controller.on('rtm_close', function(bot) {
+  console.log('** The RTM api just closed');
+  // you may want to attempt to re-open
 });
 
 
 /**
  * Core bot logic goes here!
  */
-// BEGIN EDITING HERE!
 
-controller.on('bot_channel_join', function (bot, message) {
-    bot.reply(message, "I'm here!")
+controller.on('bot_channel_join', function(bot, message) {
+  bot.reply(message, 'Hello! If you would like a problem with your coffee just say coffee problem.');
 });
 
-controller.hears('hello', 'direct_message', function (bot, message) {
-    bot.reply(message, 'Hello!');
+controller.hears('hello', 'direct_message', function(bot, message) {
+  bot.reply(message, 'Hello! If you would like a problem with your coffee just say coffee problem.');
+  bot.reply(message, 'If you would like to know the solution to a problem type solution.');
 });
 
+controller.hears('coffee problem', 'direct_message', function(bot, message) {
+  bot.startConversation(message, function(err, convo) {
+    var questions = require('./questions.js');
+    question = new questions("./problems.json");
+    convo.ask('Would you like a random problem? (yes,no, or give a specific question name)', [{
+        pattern: 'yes',
+        callback: function(response, convo) {
+          var questionName = question.getRandomQuestionName();
+          convo.say(questionName);
+          convo.say(question.getQuestion(questionName));
+          convo.next();
+        }
+      },
+      {
+        pattern: 'no',
+        callback: function(response, convo) {
+          convo.say('Goodbye');
+          convo.next();
+        }
+      },
+      {
+        default: true,
+        callback: function(response, convo) {
+          if (question.questionExists(response.text)) {
+            convo.say(question.getQuestion(response.text));
+          } else {
+            convo.say('I could not find that question.');
+            convo.repeat();
+          }
+          convo.next();
+        }
+      }
+    ]);
+  });
+});
 
-/**
- * AN example of what could be:
- * Any un-handled direct mention gets a reaction and a pat response!
- */
-//controller.on('direct_message,mention,direct_mention', function (bot, message) {
-//    bot.api.reactions.add({
-//        timestamp: message.ts,
-//        channel: message.channel,
-//        name: 'robot_face',
-//    }, function (err) {
-//        if (err) {
-//            console.log(err)
-//        }
-//        bot.reply(message, 'I heard you loud and clear boss.');
-//    });
-//});
+controller.hears('solution', 'direct_message', function(bot, message) {
+  var questions = require('./questions.js');
+  question = new questions("./problems.json");
+  bot.startConversation(message, function(err, convo) {
+    convo.ask('What is the name of the problem you would like the solution to? Say "no" to stop', [{
+      pattern: bot.utterances.no,
+      default: true,
+      callback: function(response, convo) {
+        convo.say('Goodbye');
+        convo.next();
+      },
+      pattern: '.*',
+      callback: function(response, convo) {
+        if (question.questionExists(response.text)) {
+          convo.say(question.getAnswer(response.text));
+        } else {
+          convo.say('I could not find that question.');
+          convo.repeat();
+        }
+        convo.next();
+      }
+    }]);
+  });
+});
